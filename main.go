75948d3915b7786e2a835749bb7c26f33c08235d
IlyaOsotov <soten58@yandex.ru>
2019-08-26 15:54:09 +0300

    some manipulations with maps

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("maps in Go")
+	colors := map[string]string{
+		"red":   "RED",
+		"green": "GREEN",
+	}
+
+	colors["white"] = "WHITE"
+	delete(colors, "red")
+	delete(colors, "blue")
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, upperColor := range c {
+		fmt.Println(upperColor, "is a upper of", color)
+	}
 }
